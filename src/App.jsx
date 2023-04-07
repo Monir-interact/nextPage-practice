dc0db273b186651843dd52fb5a5e4201b456edcd
A Z M Monir Hossain <94215698+Monir-interact@users.noreply.github.com>
2023-04-07 11:43:00 +0600

    practice-done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { Outlet } from "react-router-dom"
-import Header from "./components/Header"
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <h1>
-      {/* Header */}
       <Header></Header>
       <Outlet />
-      {/* Footer */}
+      <Footer />
     </h1>
-  )
+  );
 }
 
-export default App
+export default App;
